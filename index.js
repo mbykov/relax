b106023ce6c333ba6b3f52f67ba19deeada4e622
michael <m.bykov@gmail.com>
2014-01-22 05:42:08 +0400

    i

--- index.js
+++ index.js
@@ -17,13 +17,12 @@ function Relax() {
     if (!(this instanceof Relax)) return new Relax();
     // this.opts = opts({port:5984, host: '127.0.0.1', protocol: 'http:'});
     // log('O', this.opts())
-    // TODO: что же, вот так тупо перечислять методы?
-    // var parse = request.parse[this.type];
-    // return parse
-    //     ? parse(str)
-    //     : null;
-    request.dbname = dbname;
-    request.allDbs = allDbs;
+    // request.dbname = dbname;
+    // request.allDbs = allDbs;
+    // TODO: classMethods, instMethods ?
+    [dbname, allDbs].forEach(function(method) {
+        request[method.name] = method;
+    })
     return request;
 }
 
