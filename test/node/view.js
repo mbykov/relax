49ec467fed9e2c8db5c03f6142b2a2fee583a41a
michael <m.bykov@gmail.com>
2014-01-30 09:32:06 +0400

    view callback

--- test/node/view.js
+++ test/node/view.js
@@ -10,13 +10,13 @@ var admin = new Relax('http://admin:kjre4317@localhost:5984');
 //return;
 
 describe('VIEW method', function(){
-    var doc = {_id: 'some-id', text: 'some text', count: 0};
-    var other = {_id: 'other-id', text: 'some other text', count: 0};
-    var byText = function(doc) {
-        emit(doc.text, null);
-    };
+    var doc = {_id: 'some-id', text: 'some text', count: 1};
+    var other = {_id: 'other-id', text: 'some other text', count: 2};
+    var byText = function(doc) { emit(doc.text, null) };
+    var sumMap = function (doc) { emit(doc.count, doc.count) };
+    var sumReduce = function (keys, values) { return sum(values)};
 
-    var ddoc = {_id: '_design/spec', views: {'byText': {map: byText.toString()} } };
+    var ddoc = {_id: '_design/spec', views: {'byText': {map: byText.toString()}, summate:{map: sumMap.toString(), reduce: sumReduce.toString()} } };
 
     before(function(done){
         admin.create('relax-specs', function(err, res){
@@ -41,14 +41,6 @@ describe('VIEW method', function(){
                 done();
             });
     })
-    // before(function(done){
-    //     var docs = utils.makeDocs(10);
-    //     relax.dbname('relax-specs')
-    //         .push(docs, function(err, res){
-    //             done();
-    //         });
-    // })
-
     after(function(done){
         admin.drop('relax-specs', function(err, res){
             done();
@@ -75,17 +67,28 @@ describe('VIEW method', function(){
                     done();
                 });
         })
-        describe('view callback', function(){
-            it('should get docs from view', function(done){
-                relax
-                    .view('spec/byText', function(err, res) {
-                        res.length.should.equal(2);
-                        res[0].text.should.equal('some other text'); // due to collation
-                        done();
-                    })
-            })
-            // view callback with key can not be done
+        it('should get result from reduce', function(done){
+            relax
+                .view('spec/summate')
+                .query({include_docs: false})
+                .end(function(res){
+                    var value = JSON.parse(res.text).rows[0].value
+                    value.should.equal(3);
+                    done();
+                });
         })
+    })
+
+    describe('view callback', function(){
+        it('should get docs from view', function(done){
+            relax
+                .view('spec/byText', function(err, res) {
+                    res.length.should.equal(2);
+                    res[0].text.should.equal('some other text'); // due to collation
+                    done();
+                })
+        })
+        // view callback with keys or query params cannot be done
     })
 })
 
